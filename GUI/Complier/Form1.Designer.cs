f521f98df2e71dd29faed9c7f7d0fdc3dd16cb17
Mustafa Usama <mustafausamaa@gmail.com>
2023-05-20 01:16:54 +0300

    GUI files

---
+++ GUI/Complier/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Complier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1067, 291);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.VScroll += new System.EventHandler(this.richTextBox1_VScroll);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseDown);
+            this.richTextBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseMove);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 355);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(606, 287);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(624, 355);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(671, 287);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(12, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(363, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Assembly Code \"Quadruple\"";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(624, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Symbol Table";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(1125, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Compile";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(1125, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 46);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Load a file";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(1125, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 46);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(1125, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 46);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Undo Clear";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox4.Size = new System.Drawing.Size(29, 291);
+            this.richTextBox4.TabIndex = 12;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.WordWrap = false;
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1312, 654);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Compilers GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ GUI/Complier/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Complier
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        string undo = "";
+        string undo2 = "";
+        string undo3 = "";
+
+        string errorText = "";
+        Dictionary<int, string> errorlines = new Dictionary<int, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            // change text in input code box to black
+
+            if (File.Exists(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\quad.log"))
+            {
+                File.Delete(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\quad.log");
+            }
+            if (File.Exists(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\symtbl.log"))
+            {
+                File.Delete(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\symtbl.log");
+            }
+
+
+            TextWriter txt = new StreamWriter(@"..\\..\\..\\..\\input_file.compilers");
+            txt.Write(richTextBox1.Text);
+            txt.Close();
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionColor = Color.Black;
+
+            // TODO: Change path acoordingly
+            System.Diagnostics.Process.Start("cmd.exe", "/C D: & cd D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project & run.bat");
+            // wait for the process to finish and then read the output files
+            // if symtbl.log or quad.log doesnt exist wait for 1 more second
+            while (!File.Exists(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\quad.log") || !File.Exists(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\symtbl.log"))
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+
+            richTextBox2.Text = File.ReadAllText(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\quad.log");
+            richTextBox3.Text = File.ReadAllText(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\symtbl.log");
+            errorText = File.ReadAllText(@"D:\\Uni\\Spring23\\Compilers\\Project\\Compilers-Project\\error.txt");
+
+            // change text in input code box to red if there is an error
+            if (errorText != "")
+            {
+                // example error text :
+                //  e,1: expected declaration or statement at end of input
+
+                // for each line in error text file split the string by :
+                // and get the line number
+                string[] lines = errorText.Split('\n');
+                foreach (string line in lines)
+                {
+                    // we will split the line by , then by : and then get the line number
+                    string[] lineSplit = line.Split(',');
+                    string[] lineSplit2 = lineSplit[1].Split(':');
+
+
+                    // create a dictionary to store the line number and the error message
+                    // this will be used to show the error message when mouse is hovered over the line
+
+                    errorlines.Add(Int32.Parse(lineSplit2[0]), lineSplit[1]);
+
+                    int lineNumber = Int32.Parse(lineSplit2[0]);
+                    richTextBox1.Select(richTextBox1.GetFirstCharIndexFromLine(lineNumber - 1), richTextBox1.Lines[lineNumber - 1].Length);
+
+                    if (lineSplit[0] == "e")
+                    {
+                        richTextBox1.SelectionColor = Color.Red;
+                    }
+                    else if (lineSplit[0] == "w")
+                    {
+                        richTextBox1.SelectionColor = Color.Orange;
+                    }
+
+                }
+            }
+
+
+        }
+        private void richTextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.Text == "")
+                return;
+            // Get the position of the mouse.
+            Point position = new Point(e.X, e.Y);
+            // get the position of the character at the mouse position
+            int pos = richTextBox1.GetCharIndexFromPosition(position);
+            // get the line number
+            int line = richTextBox1.GetLineFromCharIndex(pos);
+            // select the line and check if color is red
+            richTextBox1.Select(richTextBox1.GetFirstCharIndexFromLine(line), richTextBox1.Lines[line].Length);
+            if (richTextBox1.SelectionColor == Color.Red)
+            {
+                // get the error message from the dictionary
+                string errorMessage = errorlines[line + 1];
+
+                // if color is red then show the error message
+                toolTip1.Show(errorMessage, richTextBox1, e.X, e.Y, 5000);
+
+            }
+            else if (richTextBox1.SelectionColor == Color.Orange)
+            {
+                // get the error message from the dictionary
+                string errorMessage = errorlines[line + 1];
+
+                // if color is orange then show the warning message
+                toolTip1.Show(errorMessage, richTextBox1, e.X, e.Y, 5000);
+            }
+            else
+            {
+                toolTip1.Hide(richTextBox1);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+                return;
+            undo = richTextBox1.Text;
+            undo3 = richTextBox3.Text;
+            undo2 = richTextBox2.Text;
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = undo;
+            richTextBox2.Text = undo2;
+            richTextBox3.Text = undo3;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Clear the line numbers
+            richTextBox4.Clear();
+            if (richTextBox1.Text == "")
+            {
+                AddLineNumbers();
+
+            }
+            AddLineNumbers();
+
+        }
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_VScroll(object sender, EventArgs e)
+        {
+            richTextBox4.Text = "";
+            AddLineNumbers();
+            richTextBox4.Invalidate();
+        }
+        public void AddLineNumbers()
+        {
+            // create & set Point pt to (0,0)
+            Point pt = new Point(0, 0);
+            // get First Index & First Line from richTextBox1
+            int First_Index = richTextBox1.GetCharIndexFromPosition(pt);
+            int First_Line = richTextBox1.GetLineFromCharIndex(First_Index);
+            // set X & Y coordinates of Point pt to ClientRectangle Width & Height respectively
+            pt.X = ClientRectangle.Width;
+            pt.Y = ClientRectangle.Height;
+            // get Last Index & Last Line from richTextBox1
+            int Last_Index = richTextBox1.GetCharIndexFromPosition(pt);
+            int Last_Line = richTextBox1.GetLineFromCharIndex(Last_Index);
+            // set Center alignment to LineNumberTextBox
+            richTextBox4.SelectionAlignment = HorizontalAlignment.Center;
+            // set LineNumberTextBox text to null & width to getWidth() function value
+            richTextBox4.Text = "";
+            richTextBox4.Width = getWidth();
+            // now add each line number to LineNumberTextBox upto last line
+            for (int i = First_Line; i <= Last_Line; i++)
+            {
+                richTextBox4.Text += i + 1 + "\n";
+            }
+        }
+        public int getWidth()
+        {
+            int w = 25;
+            // get total lines of richTextBox1
+            int line = richTextBox1.Lines.Length;
+
+            if (line <= 99)
+            {
+                w = 20 + (int)richTextBox1.Font.Size;
+            }
+            else if (line <= 999)
+            {
+                w = 30 + (int)richTextBox1.Font.Size;
+            }
+            else
+            {
+                w = 50 + (int)richTextBox1.Font.Size;
+            }
+
+            return w;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddLineNumbers();
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            Point pt = richTextBox1.GetPositionFromCharIndex(richTextBox1.SelectionStart);
+            if (pt.X == 1)
+            {
+                AddLineNumbers();
+            }
+        }
+
+        private void richTextBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Select();
+            richTextBox4.DeselectAll();
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
